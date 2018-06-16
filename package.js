c39e9d63c7024195b8099b2b615717722b704cc3
laperez <laperez@grupoclece.com>
2018-06-16 12:13:54 +0200

    Adds constraint version for tempating packge

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2.6');
   api.use('ecmascript');
-  api.use('templating');
+  api.use('templating@1.3.2');
 
   api.addFiles(
     [
